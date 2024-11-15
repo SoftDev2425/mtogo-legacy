4bce0cd97e70d51ffff43dd9ec5e270dbec5f53a
Owais Dashti <owaisdashti@outlook.com>
2024-11-15 20:30:50 +0100

    cors

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,17 @@
-import express from 'express';
+import express, { Response } from 'express';
 import authController from '../controllers/auth.controller';
+import { validateSession } from '../middlewares/sessions';
+import { CustomRequest } from '@/types/CustomRequest';
 
 const router = express.Router();
+
+router.get(
+  '/validate',
+  validateSession,
+  (req: CustomRequest, res: Response) => {
+    res.status(200).json({ message: 'Session is valid', email: req.email });
+  },
+);
 
 router.post('/login', authController.handleLogin);
 
