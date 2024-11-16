707cb58822201bed7228b8bf50121b788d3b0a5c
Andreas Fritzbøger <andreasfritzboger@live.dk>
2024-11-16 16:56:46 +0100

    Eslint

--- src/utils/populateDBWithAdmin.ts
+++ src/utils/populateDBWithAdmin.ts
@@ -4,42 +4,41 @@ import readline from 'readline';
 
 // Create readline interface for user input
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 async function populateAdminToDB(email: string, password: string) {
-    try {
-        await prisma.admins.create({
-            data: {
-                email,
-                password: await bcrypt.hash(password, 10)
-            }
-        });
-        console.log('Admin added successfully!');
-    } catch (error) {
-        console.error('Error populating database:', error);
-        
-    } finally {
-        await prisma.$disconnect();
-    }
+  try {
+    await prisma.admins.create({
+      data: {
+        email,
+        password: await bcrypt.hash(password, 10),
+      },
+    });
+    console.log('Admin added successfully!');
+  } catch (error) {
+    console.error('Error populating database:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 function askQuestion(query: string): Promise<string> {
-    return new Promise((resolve) => rl.question(query, resolve));
+  return new Promise(resolve => rl.question(query, resolve));
 }
 
 async function main() {
-    try {
-        const email = await askQuestion('Enter admin email: ');
-        const password = await askQuestion('Enter admin password: ');
+  try {
+    const email = await askQuestion('Enter admin email: ');
+    const password = await askQuestion('Enter admin password: ');
 
-        await populateAdminToDB(email, password);
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        rl.close();
-    }
+    await populateAdminToDB(email, password);
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    rl.close();
+  }
 }
 
 main();
